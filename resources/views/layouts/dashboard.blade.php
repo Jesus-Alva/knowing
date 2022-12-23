7adb84d8f98bcc011373347c8693b2d0b8396245
Jesus-Alva <57468191+Jesus-Alva@users.noreply.github.com>
2022-12-22 21:00:24 -0600

    Knowing system Anexo componentes para publicaciones

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -65,22 +65,6 @@
     <section>
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
         
-        @if ($posts->count())
-         
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 rounded-md">
-            @foreach ($posts as $post)
-                <div>
-                    <a href="{{ route('posts.show',  ['post' => $post, 'user' => $user]) }}">
-                        <img class="rounded-xl shadow-md" src="{{ asset('uploads') . '/' . $post->imagen}}" alt="imagen del post {{$post->titulo}}">
-                    </a>
-                </div>
-            @endforeach
-        </div>
-        <div class="m-10">
-            {{ $posts->links() }}
-        </div>
-        @else
-            <a class="text-gray-600 uppercase text-sm flex justify-center font-bold">Aun no hay posts</a>
-        @endif
+        <x-listar-post :posts="$posts"/>
     </section>
 @endsection
