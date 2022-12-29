abaf584b2705e89a313c7a8730b41c451148f151
Jesus-Alva <57468191+Jesus-Alva@users.noreply.github.com>
2022-12-28 19:26:16 -0600

    AppServiceProvider con https

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, ' store']);
+Route::post('/login', [LoginController::class, 'store']);
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 // Rutas para perfil
